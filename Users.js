08c6bcce2f50d05e652f0d36cb7d1ed32650e649
mrobin33 <mrobin33@students.towson.edu>
2017-05-11 15:42:19 -0400

    Add files via upload
    Added user input.

--- Users.js
+++ Users.js
@@ -1,29 +1,37 @@
 // Users Class
 var user = {name:"", email:"", username:"", password:"", phone:0};
-function resetPass(user, newPassword){
+function createUser(name, email, username, password, phone){
 	"use strict";
-	user.password = newPassword;
-	if(newPassword === user.password){
+	document.getelementbyID("user") = {name, email, username, password, phone};
+}
+function resetPass(newPassword){
+	"use strict";
+	newPassword = document.getelementbyID("newPassword");
+	while(newpPassword === user.password){
+
 		window.alert("New password cannot be the same as the current password");
 	}
+	user.password = newPassword;
 }
+function adminUpdateInfo(admin, newEmail, newUsername, newPhone){
+	"use strict";
+	user.email = document.getelementbyID("newEmail");
+	user.username = document.getelementbyID("newUsername");
+	user.phone = document.getelementbyID("newPhone"); 
+}
+
 function updateInfo(user, newEmail, newUsername, newPhone){
 	"use strict";
-	user.email= newEmail;
-	user.username = newUsername;
-	user.phone = newPhone; 
+	newEmail = document.getelementbyID("user.email");
+	newUsername = document.getelementbyID("user.username");
+	newPhone = document.getelementbyID("user.phone"); 
 }
 var admin = {name:"", email:"", username:"", password:"", phone:0 , ID: 0};
 function resetPass2(admin, newPassword){
 	"use strict";
-	admin.password = newPassword;
+	newPassword = document.getElementById(admin.password);
 	if(newPassword === admin.password){
 		window.alert("New password cannot be the same as the current password");
 	}
 }
-function updateInfo2(admin, newEmail, newUsername, newPhone){
-	"use strict"
-	user.email= newEmail;
-	user.username = newUsername;
-	user.phone = newPhone; 
-}
+
